80045a60e2a23411b08d91ec6cdac767522bd05d
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-18 16:31:09 +0530

    backed to add books

--- librarian/add_books.php
+++ librarian/add_books.php
@@ -1,47 +1,103 @@
 <?php
+session_start();
 include "connection.php";
-include"header.php";
+include "header.php";
+
 ?>
 
-        <!-- page content area main -->
-        <div class="right_col" role="main">
-            <div class="">
-                <div class="page-title">
-                    <div class="title_left">
-                        <h3>Plain Page</h3>
-                    </div>
+<!-- page content area main -->
+<div class="right_col" role="main">
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                <h3>Plain Page</h3>
+            </div>
 
-                    <div class="title_right">
-                        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="clearfix"></div>
-                <div class="row" style="min-height:500px">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="x_panel">
-                            <div class="x_title">
-                                <h2>Add Books Info</h2>
-
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="x_content">
-                                Add content to the page ...
-                            </div>
-                        </div>
+            <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- /page content -->
 
-        <?php
-include"footer.php";
+        <div class="clearfix"></div>
+        <div class="row" style="min-height:500px">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>Add Books Info</h2>
+
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <form name="form1" action="" method="POST" class="col-sm-6" enctype="multipart/form-data">
+
+                            <table class="table table-bordered">
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Book name" name="books_name" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="file" class="form-control" placeholder="Username" name="books_image" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Author name" name="books_author_name" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Publication name" name="books_publication_name" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Purchase date" name="books_purchase_date" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Book price" name="books_price" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Book quantity" name="books_qty" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Available quantity" name="available_qty" required""></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="submit" class="btn btn-primary submit" placeholder="Book quantity" name="submit1" value="insert books details"></td>
+                                </tr>
+                            </table>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /page content -->
+
+
+<?php
+
+if (isset($_POST["submit1"])) {
+    $tm = md5(time());
+    $fnm = $_FILES["f1"]["name"];
+    $dst = "./books_image/" . $tm . $fnm;
+    $dst1 = "./books_image/" . $tm . $fnm;
+    move_uploaded_file($_FILES["f1"]["tmp_name"], $dst);
+
+    mysqli_query($link, "INSERT INTO `add_books` ( `books_name`, `books_image`, `books_author_name`, `books_publication_name`, `books_purchase_date`, `books_price`, `books_qty`, `available_qty`, `librarian_username`) VALUES ( '$_POST[books_name]', '$dst1]', '$_POST[books_author_name]', '$_POST[books_publication_name]', '$_POST[books_purchase_date]', '$_POST[books_price]', '$_POST[books_qty]', '$_POST[available_qty]', ' $_SESSION[librarian]')");
+
+
+    ?>
+<script type="text/javascript">
+    alert("Book inserted successfully");
+</script>
+<?php
+}
+
+    ?>
+<br>
+<?php
+include "footer.php";
 ?>
--- librarian/login.php
+++ librarian/login.php
@@ -1,70 +1,72 @@
 <?php
+session_start();
 include "connection.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Librarian Login Form | LMS </title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/animate.min.css" rel="stylesheet">
-    <link href="css/custom.min.css" rel="stylesheet">
-</head> 
+        <title>Librarian Login Form | LMS </title>
 
-<br>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/animate.min.css" rel="stylesheet">
+        <link href="css/custom.min.css" rel="stylesheet">
+    </head>
 
-<div class="col-lg-12 col-md-12 col-sm-12 text-center ">
-    <h1 style="font-family:Lucida Console">Library Management System</h1>
-</div>
+    <br>
 
-<br>
+    <div class="col-lg-12 col-md-12 col-sm-12 text-center ">
+        <h1 style="font-family:Lucida Console">Library Management System</h1>
+    </div>
 
-<body class="login">
+    <br>
 
-
-<div class="login_wrapper">
-
-    <section class="login_content">
-        <form name="form1" action="" method="post">
-            <h1>Librarian Login Form</h1>
-
-            <div>
-                <input type="text" name="username" class="form-control" placeholder="Username" required=""/>
-            </div>
-            <div>
-                <input type="password" name="password" class="form-control" placeholder="Password" required=""/>
-            </div>
-            <div>
-
-                <input class="btn btn-default submit" type="submit" name="submit1" value="Login">
-                <a class="reset_pass" href="#">Lost your password?</a>
-            </div>
-
-            <div class="clearfix"></div>
-
-            <div class="separator">
-                <p class="change_link">New to site?
-                    <a href="registration.html"> Create Account </a>
-                </p>
-
-                <div class="clearfix"></div>
-                <br/>
+    <body class="login">
 
 
-            </div>
-        </form>
-    </section>
+        <div class="login_wrapper">
+
+            <section class="login_content">
+                <form name="form1" action="" method="post">
+                    <h1>Librarian Login Form</h1>
+
+                    <div>
+                        <input type="text" name="username" class="form-control" placeholder="Username" required="" />
+                    </div>
+                    <div>
+                        <input type="password" name="password" class="form-control" placeholder="Password" required="" />
+                    </div>
+                    <div>
+
+                        <input class="btn btn-default submit" type="submit" name="submit1" value="Login">
+                        <a class="reset_pass" href="#">Lost your password?</a>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="separator">
+                        <p class="change_link">New to site?
+                            <a href="registration.html"> Create Account </a>
+                        </p>
+
+                        <div class="clearfix"></div>
+                        <br />
 
 
-</div>
-<?php
+                    </div>
+                </form>
+            </section>
+
+
+        </div>
+        <?php
 if(isset($_POST["submit1"]))
 {
     $count = 0;
@@ -77,14 +79,16 @@ if(isset($_POST["submit1"]))
         <div class="alert alert-danger col-lg-12 col-md-6 col-sm-12 col-lg-push-0 text-center m-auto ">
             <strong style="color:white">Invalid</strong> Username Or Password.
         </div>
-            <?php
-    }else{
+        <?php
+    }
+    
+    else{
+        $_SESSION["librarian"]=$_POST["username"];
 ?>
-<script type="text/javascript">
-
-window.location="student_info.php";
-</script>
-<?php
+        <script type="text/javascript">
+        window.location = "student_info.php";
+        </script>
+        <?php
     }
 
 
@@ -96,5 +100,6 @@ window.location="student_info.php";
 
 
 
-</body>
+    </body>
+
 </html>
