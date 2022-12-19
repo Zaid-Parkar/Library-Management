7ae68bf92c3015d4cef87a0d208687841c6c2b6d
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-19 22:28:13 +0530

    issue book backend update

--- librarian/issue_books.php
+++ librarian/issue_books.php
@@ -41,7 +41,7 @@ include "connection.php";
                                         <select name="enr" class="form-control selectpicker" style="margin: 3px;">
                                             <?php
                                             $res = mysqli_query($link, "SELECT enrollment FROM `student_registration` ");
-                                          
+
                                             while ($row = mysqli_fetch_array($res)) {
                                                 echo "<option>";
                                                 echo $row["enrollment"];
@@ -57,76 +57,96 @@ include "connection.php";
 
                             </table>
 
-                        
 
-                        <?php
 
-                        if (isset($_POST["submit1"])) {
-                            $res1 = mysqli_query($link, "select * from student_registration where enrollment='$_POST[enr]'");
-                        
-                            while ($row1 = mysqli_fetch_array($res1)) {
-
-                                $firstname=$row1["firstname"];
-                                $lastname=$row1["lastname"];
-                                $username=$row1["username"];
-                                $email=$row1["email"];
-                                $contact=$row1["contact"];
-                                $sem=$row1["sem"];
-                                $enrollment=$row1["enrollment"];
-                            }
-                        ?>
-                        <table class="table table-bordered">
-                            <tr>
-                                <td><input type="text" class="form-control" placeholder="Enrollment number" name="enrollment" value="<?php echo "$enrollment"; ?>" disabled></td>
-                            </tr>
-                            <tr>
-                                <td><input type="text" class="form-control" placeholder="Student name" name="s_name" value="<?php echo $firstname . ' ' . $lastname;?>"></td>
-                            </tr>
-                            <tr>
-                                <td><input type="text" class="form-control" placeholder="Student sem" name="s_sem" value="<?php echo "$sem";?>"></td>
-                            </tr>
-                            <tr>
-                                <td><input type="text" class="form-control" placeholder="Student contact" name="s_contact" value="<?php echo "$contact"; ?>"></td>
-                            </tr>
-                            <tr>
-                                <td><input type="text" class="form-control" placeholder="Student email" name="s_email" value="<?php echo "$email"; ?>"></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <select name="booksname" class="form-control  selectpicker">
-
-                                        <?php
-                            $res = mysqli_query($link, "select books_name from add_books");
-                            while ($row = mysqli_fetch_array($res)) {
-                                echo "<option>";
-                                echo $row["books_name"];
-                                echo "</option>";
-                            }
-                                        ?>
-                                    </select>
-                                </td>
-                            </tr>
-                        <tr>
-                            <td>
-                            <input type="text" class="form-control" placeholder="Issue date" name="books_issue_date" value="<?php echo date("d-m-Y");  ?>" disabled>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                            <input type="text" class="form-control" placeholder="User name" name="s_username" value="<?php echo $username; ?>" disabled>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td >
-                            <input type="submit" class="form-control  btn btn-primary" name="submit2" value="Issue book" >
-                            </td>
-                        </tr>
-                        
-                        </table>
                             <?php
-                        }
+
+                            if (isset($_POST["submit1"])) {
+                                $res1 = mysqli_query($link, "select * from student_registration where enrollment='$_POST[enr]'");
+
+                                while ($row1 = mysqli_fetch_array($res1)) {
+
+                                    $firstname = $row1["firstname"];
+                                    $lastname = $row1["lastname"];
+                                    $susername = $row1["username"];
+                                    $email = $row1["email"];
+                                    $contact = $row1["contact"];
+                                    $sem = $row1["sem"];
+                                    $enrollments = $row1["enrollment"];
+                                }
+                            ?>
+                            <table class="table table-bordered">
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Enrollment number" name="enrollment" value="<?php echo $enrollments; ?>"></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Student name" name="s_name" value="<?php echo $firstname . ' ' . $lastname; ?>"></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Student sem" name="s_sem" value="<?php echo "$sem"; ?>"></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Student contact" name="s_contact" value="<?php echo "$contact"; ?>"></td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Student email" name="s_email" value="<?php echo "$email"; ?>"></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <select name="booksname" class="form-control  selectpicker">
+
+                                            <?php
+                                $res = mysqli_query($link, "select books_name from add_books");
+                                while ($row = mysqli_fetch_array($res)) {
+                                    echo "<option>";
+                                    echo $row["books_name"];
+                                    echo "</option>";
+                                }
+                                            ?>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="text" class="form-control" placeholder="Issue date" name="book_issue_date" value="<?php echo date("d-m-Y"); ?>">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="text" class="form-control" placeholder="Return date" name="books_return_date" value="<?php echo date('d-m-Y ', strtotime("+7 day")); ?>">
+
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="text" class="form-control" placeholder="User name" name="username" value="<?php echo $susername; ?>">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <input type="submit" class="form-control  btn btn-primary" name="submit2" value="Issue book">
+                                    </td>
+                                </tr>
+
+                            </table>
+                            <?php
+                            }
                             ?>
                         </form>
+
+                        <?php
+                        if (isset($_POST["submit2"])) {
+                            $link = mysqli_query($link, "INSERT INTO `issue_books` (`student_enrollment`, `student_name`, `student_sem`, `student_contact`, `student_email`, `books_name`, `books_issue_date`, `books_return_date`, `student_username`) VALUES ( '$_POST[enrollment]', '$_POST[s_name]', '$_POST[s_sem]', '$_POST[s_contact]', '$_POST[s_email]', '$_POST[booksname]', '$_POST[book_issue_date]', '$_POST[books_return_date]','$_POST[username]' )");
+                        ?>
+                        <script type="text/javascript">
+                            alert("Books issued successfully");
+                            window.location.href = window.location.href;
+                        </script>
+                        <?php
+
+                        }
+
+                        ?>
                     </div>
                 </div>
             </div>
