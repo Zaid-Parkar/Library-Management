b9818cdc32e23677eb6cd94fe7ab8f20e2abebdd
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-24 22:56:27 +0530

    return book backend update

--- librarian/return.php
+++ librarian/return.php
@@ -3,6 +3,13 @@ include "connection.php";
 $id = $_GET["id"];
 $a = date("d-m-Y");
 $res = mysqli_query($link, "update issue_books set books_return_date='$a' where id=$id;");
+$res = mysqli_query($link, "select * from issue_books where id=$id");
+while($row=mysqli_fetch_array($res))
+{
+    $books_name = $row["books_name"];
+}
+mysqli_query($link, "UPDATE add_books set available_qty=available_qty+1 where books_name='$books_name'");
+
 
 
 ?>
